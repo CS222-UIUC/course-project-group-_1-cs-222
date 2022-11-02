1de61225f7c89d31d1198d4fb150ba3497e33d67
Varun Giridhar <32874672+varungiridhar@users.noreply.github.com>
2022-11-02 14:47:23 -0500

    added map view

--- community-map/community-map/ContentView.swift
+++ community-map/community-map/ContentView.swift
@@ -14,9 +14,14 @@ public var onGoing = false
 var valuesArray: [(Double, Double, Double)] = []
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
     var body: some View {
+        
         Text("left").position(x:100, y:600)
         Text("right").position(x:300, y:217)
+        Map(coordinateRegion: $region).frame(width: 400, height: 300).position(x:0, y:0)
+        
     }
 }
 
