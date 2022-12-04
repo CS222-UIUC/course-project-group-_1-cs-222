6c4d6ffe67093a898a6781d6cc24cfd03e529499
Varun Giridhar <32874672+varungiridhar@users.noreply.github.com>
2022-12-04 13:04:59 -0600

    working version 1

--- community-map/community-map/ContentView.swift
+++ community-map/community-map/ContentView.swift
@@ -14,14 +14,44 @@ public var onGoing = false
 var valuesArray: [(Double, Double, Double)] = []
 
 struct ContentView: View {
-    Motion motion = new Motion()
-    motion.startAccelerometer();
-    motion.startGyros();
+    
+    
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
+    @State private var motionClass = Motion()
+    @State private var message = "-"
 
     var body: some View {
+        
+        VStack {
+                    Button("Run") {
+                        print("Tapped!")
+                        
+                        OperationQueue().addOperation {
+                            for i in 0 ... 100 {
+
+                                OperationQueue.main.addOperation {
+                                    motionClass.startAccelerometer()
+                                    motionClass.startGyros()
+                                    self.message = String(motionClass.valuesArray.first?.0 ?? 0.0)
+                                    print(i, self.message)
+                                    
+                                }
+                                sleep(1)
+                            }
+
+                        }
+                    }
+
+                    Text(message)
+                        .frame(width: 100)
+                        .padding()
+                }
+                .padding(40)
+        
         Map(coordinateRegion: $region).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        Text(valuesArray).frame(
+        Text("center").frame(
+            
             maxWidth: .infinity,
             maxHeight: .infinity,
             alignment: .bottom)
@@ -31,6 +61,7 @@ struct ContentView: View {
         
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
