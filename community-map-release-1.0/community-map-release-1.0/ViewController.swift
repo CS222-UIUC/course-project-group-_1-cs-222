14dc8427af69accbf7726a3bd3b7a63359eee99c
Kushal Agarwal <67228796+kushal238@users.noreply.github.com>
2022-12-12 12:39:18 -0600

    Final upload - community-map-release-1.0

---
+++ community-map-release-1.0/community-map-release-1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  community-map-release-1.0
+//
+//  Created by Kushal Agarwal on 12/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
